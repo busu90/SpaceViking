4c81c6f249dda989b4de7cbe40a04f96844a0765
Andrei <abusu90@yahoo.com>
2012-11-17 16:07:41 +0200

    add background

---
+++ SpaceViking/BackgroundLayer.h
@@ -0,0 +1,13 @@
+//
+//  BackgroundLayer.h
+//  SpaceViking
+//
+//  Created by Andrei Busuioc on 11/17/12.
+//
+//
+
+#import "CCLayer.h"
+#import "cocos2d.h"
+@interface BackgroundLayer : CCLayer
+
+@end
